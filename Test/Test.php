ea6c6a6c63ee55a14c4e81ba57a356993400e321
Rayan BELOUCIF <rayan.beloucif16@imperial.ac.uk>
2017-02-12 19:02:28 +0000

    CW CI

--- Test/Test.php
+++ Test/Test.php
@@ -2,7 +2,7 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testPrime() { //A test for the prime function in the index.php file
-		include "./../index.php";
+		include "index.php";
 		for($i=2;$i<$mPrime;$i++){ // For all smaller integer then $mprime : check whether it's a divider or not
 			$this->assertTrue($mPrime%$i!=0); //If one integer is a divider then the test script fails
 		}
