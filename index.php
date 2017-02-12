65f83171e8f06858d0a2806b5a54c0b5fa4911e6
Rayan BELOUCIF <rayan.beloucif16@imperial.ac.uk>
2017-02-12 18:54:48 +0000

    yo

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+function return_prime(){
+	return 31;
+}
+
+
+$mPrime = return_prime();
+
+echo "<h>C475 Coursework No4 - Rayan BELOUCIF & Adam BALOGH</h>";
+echo "<br/>";
+echo "$mPrime is a prime!";
+
+
+
+
+
+
+
+
+
+?>
